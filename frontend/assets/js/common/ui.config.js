f6b9205f059f59ed1952c7e58ef09e0b279dd53f
Nastoiker <damirdambinov@gmail.com>
2022-11-26 23:26:05 +0400

    jsstart

---
+++ frontend/assets/js/common/ui.config.js
@@ -0,0 +1,5 @@
+export const components = {
+    form: document.forms['loginForm'],
+    inputEmail: document.getElementById('email'),
+    inputPassword: document.getElementById('password'),
+};
--- frontend/assets/js/services/script.js
+++ frontend/assets/js/services/script.js
@@ -1,3 +1,4 @@
+import components from '../common/ui.config';
 async function getBooks() {
     return new Promise(resolve => fetch('http://bookservice:88/books').then(e => e.json()).then(res => setTimeout(3000, resolve(res))));
 }
