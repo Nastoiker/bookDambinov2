aef0ff533f5929855f8dadeead1df0c642ef98a7
Nastoiker <damirdambinov@gmail.com>
2022-12-04 23:57:25 +0400

    wion

--- frontend/assets/js/book_genre.js
+++ frontend/assets/js/book_genre.js
@@ -35,12 +35,31 @@ const showBook = (arr) => {
         container_book.innerHTML +=` <div class="card_book" data-pos="${book.id}" data-rating="${book.rating["COUNT(rating)"]}" data-avg-rating="${book.rating['round(AVG(rating),1)']}">
                 <div class="info_book">
                     <div class="header-card">
+                        
+                           <div  style="display: flex; align-items: center;">
+                        <svg width="0" height="0" viewBox="0 0 32 32">
+  <defs>
+    <linearGradient id="half${book.id}" x1="0" x2="100%" y1="0" y2="0">
+      <stop offset="${(book.rating['round(AVG(rating),1)'] / 5) * 100}%" stop-color="#fed94b"></stop>
+      <stop offset="${(book.rating['round(AVG(rating),1)'] / 5) * 100}%" stop-color="#f7efc5"></stop>
+    </linearGradient>
+    
+    <symbol viewBox="0 0 32 32" id="star${book.id}">
+      <path d="M31.547 12a.848.848 0 00-.677-.577l-9.427-1.376-4.224-8.532a.847.847 0 00-1.516 0l-4.218 8.534-9.427 1.355a.847.847 0 00-.467 1.467l6.823 6.664-1.612 9.375a.847.847 0 001.23.893l8.428-4.434 8.432 4.432a.847.847 0 001.229-.894l-1.615-9.373 6.822-6.665a.845.845 0 00.214-.869z" />
+    </symbol>
+  </defs>
+</svg>
+                            
+
+<!-- Пример применения звезды с наложением градиента -->
+    <svg class="c-icon" width="32" height="32" viewBox="0 0 32 32">
+        <use xlink:href="#star${book.id}" fill="url(#half${book.id})"></use>
+    </svg>
+
+                            <span style="margin-left: 10px">${book.rating['round(AVG(rating),1)'] ?? 0}</span>
+                            </div>                        
                         <div>
-                            <img src="assets/src/icons/star.svg" alt="star">
-                            <span>${book.rating['round(AVG(rating),1)']}</span>
-                        </div>
-                        <div>
-                            <span>${book.rating["COUNT(rating)"]}</span>
+                            <span>${book.rating["COUNT(rating)"] ?? 0}</span>
                             <span>отзыв.</span>
                         </div>
                     </div>
